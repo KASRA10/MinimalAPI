c2750c0a6f99d0b08053eff1483f6dc649ecdbee
kasra10 <kasrasparrow@gmail.com>
2025-06-02 01:18:29 +0330

    add Get endpoints

--- MinimalAPI/Documentation/Developers.Help.md
+++ MinimalAPI/Documentation/Developers.Help.md
@@ -27,3 +27,49 @@ We hae 2 types of section (static|dynamic):
 **Static** = "/profiles/kasra
 
 **Dynamic** = /profiles/{userName}" **==>** Delegate should access to that dynamic section
+
+## static Pattern | Path | Address
+
+`app.MapGet("/browsers", static () => "Edge\nChrome\nFireFox\nOpera\nArc\nVivaldi\nSafari");`
+
+OutPut:
+
+```text
+Edge
+Chrome
+FireFox
+Opera
+Arc
+Vivaldi
+Safari
+```
+
+## Dynamic Pattern | Path | Address
+
+**FromRoute == Dynamic Section on Path | pattern** ==> it adds on path.
+
+`app.MapGet(
+    "/api/{companyName}/priority",
+    ([FromRoute] string companyName) => $"I like To Work In \"{companyName}\".")
+`
+
+EX:
+
+**Endpoint:** <http://localhost:5020/api/Microsoft/priority>
+
+OutPut:
+
+`I like To Work In "Microsoft".`
+
+**FromQuery** ===> mean with ? on path be added, data will added on path | route | pattern
+
+**_? Parameter Name = Value.....separator: &_**
+
+EX:
+
+`app.MapGet(
+"/api/{brand}",
+([FromRoute] string brand, [FromQuery] string point, [FromQuery] string alphabet) =>
+$"Parent: {brand}\nComment on: {point}\nstar value: \"{alphabet}\"");`
+
+<http://localhost:5020/api/Nikee?point=Perfect&alphabet=A>
--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,5 +7,26 @@ var app = builder.Build();
 app.MapGet("/", () => "Application Name: KasraK10 Minimal API - Basic\nList Of EndPoints:");
 
 //? Get ==> Get Method, MapGet("Path| Pattern", Delegate| Lambda Function);
+//* static Pattern | Path | Address
+app.MapGet("/browser", static () => "Edge\nChrome\nFireFox\nOpera\nArc\nVivaldi\nSafari");
+
+//? Dynamic Path | Pattern
+//* FromRoute == Dynamic Section on Path | pattern
+app.MapGet(
+    "/api/{companyName}/priority",
+    ([FromRoute] string companyName) => $"I like To Work In \"{companyName}\"."
+);
+
+//? FromQuery ===> mean with ? on path be added, data will added on path | route | pattern
+//* http://localhost:5020/api/priceItems?itemName=PS5&state=Hot
+//! Parameter Name = Value.....separator: &
+app.MapGet(
+    "/api/{brand}",
+    ([FromRoute] string brand, [FromQuery] string point, [FromQuery] string alphabet) =>
+        $"Parent: {brand}\nComment on: {point}\nstar value: \"{alphabet}\""
+);
+
+//? Post ==> Send Data
+//? Classic HTML Form ==> FromForm
 
 app.Run();
